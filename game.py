0781abb318712a21d7d3336f731ca78c1743e950
Jamie Fujimoto <hayntopdawg@gmail.com>
2015-05-18 00:33:06 -0400

    Create game.py

---
+++ game.py
@@ -0,0 +1,22 @@
+__author__ = 'FujNasty'
+
+def deal():
+    pass
+
+def hit():
+    pass
+
+def stand():
+    pass
+
+def double_down():
+    pass
+
+def split():
+    pass
+
+def insurance():
+    pass
+
+def surrender():
+    pass
