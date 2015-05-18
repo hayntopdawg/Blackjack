dc984e463a31128aaac5d594d0ad38e217528141
Jamie Fujimoto <hayntopdawg@gmail.com>
2015-05-18 01:53:47 -0400

    Create test_hand.py

---
+++ test_hand.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+__author__ = 'FujNasty'
+
+
+class TestHand(TestCase):
+    def test_get_value(self):
+        from players import Hand
+        from cards import Card
+        test_hand = Hand()
+        test_hand.add_card(Card("9", "Spades"))
+        test_hand.add_card(Card("Ace", "Spades"))
+        self.assertEquals(test_hand.get_value(), 20)
+        test_hand.add_card(Card("Ace", "Hearts"))
+        self.assertEquals(test_hand.get_value(), 21)
+        test_hand.add_card(Card("Ace", "Clubs"))
+        self.assertEquals(test_hand.get_value(), 12)
