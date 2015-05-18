da8179709d5efea8d6e839f987cefa8e486e722b
Jamie Fujimoto <hayntopdawg@gmail.com>
2015-05-18 11:11:55 -0400

    Create deal_game()

--- game.py
+++ game.py
@@ -1,7 +1,19 @@
 __author__ = 'FujNasty'
 
-def deal():
-    pass
+from cards import Deck
+from players import Player, Dealer
+
+def deal_game():
+    deck = Deck()
+    deck.shuffle()
+    player = Player()
+    dealer = Dealer()
+    player.add_card(deck.deal())
+    dealer.add_card(deck.deal())
+    player.add_card(deck.deal())
+    dealer.add_card(deck.deal())
+    print("Player has a %s and a %s (%d)" % (player.hand[0], player.hand[1], player.get_value()))
+    print("Dealer is showing a %s (%d)" % (dealer.hand[0], dealer.get_value_first_card()))
 
 def hit():
     pass
